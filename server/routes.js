525b28cebe9b9b1158a3de28fcf3451fa9915968
homa <homa@redhat.com>
2018-01-24 11:07:32 -0500

    removed un-needed code

--- server/routes.js
+++ server/routes.js
@@ -11,14 +11,9 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/workstreams', require('./api/workstream'));
 
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|components|app|bower_components|assets)/*')
-   .get(errors[404]);
-
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
-
       res.sendFile(path.join(__dirname, '../' + app.get('appPath') + '/index.html'));
     });
 };
