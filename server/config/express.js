6da8449822b727624b6337e3e19391d6d4c70267
homa <homa@redhat.com>
2018-01-23 23:41:33 -0500

    fixed a bug

--- server/config/express.js
+++ server/config/express.js
@@ -27,8 +27,8 @@ module.exports = function(app) {
   app.use(cookieParser());
 
   if ('production' === env) {
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', config.root + '/client');
     app.use(morgan('dev'));
   }
 
