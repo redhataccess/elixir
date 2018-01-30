a27622b5033ab9a638f19d0d9638a0a9a334c53c
homa <homa@redhat.com>
2018-01-30 13:59:07 -0500

    moved all frontend routers to a file

--- client/app/app.js
+++ client/app/app.js
@@ -8,11 +8,21 @@ angular.module('elixirApp', [
   'colorpicker.module',
   'ui.bootstrap.datetimepicker'
 ])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .otherwise({
-        redirectTo: '/'
-      });
+.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/main/main.html',
+      controller: 'MainCtrl',
+      reloadOnSearch: false
+    })
+    .when('/admin', {
+          templateUrl: 'app/admin/views/index.html',
+          controller: 'WorkstreamIndexController',
+          reloadOnSearch: false
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 
-    $locationProvider.html5Mode(true);
-  });
+  $locationProvider.html5Mode(true);
+});
